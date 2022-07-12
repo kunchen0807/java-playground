e3b8c376b69372dfdb6649deef59a9b9a7574bfe
Kun Chen <justinchen9387@gmail.com>
2022-07-12 11:09:08 -0700

    read command line arguments practice

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,6 +2,17 @@ package com.galvanize;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+        } else {
+            String name = args[0];
+            if (args.length < 2) {
+                System.out.println("Please specify an email for " + name);
+            } else {
+                String email = args[1];
+                System.out.println(name + " <" + email + ">");
+            }
+        }
     }
 }
